6d21dbc4ef4942d313cce5253a0000ee1b1c6cc7
estraw1059 <10673424+estraw1059@users.noreply.github.com>
2020-06-18 21:34:43 -0500

    Get function fixed. Deployed

--- get.js
+++ get.js
@@ -1,8 +1,7 @@
 import handler from "./libs/handler-lib";
 import dynamoDb from "./libs/dynamodb-lib";
 
-export const main = handler(async (event, context) =>
-{
+export const main = handler(async (event, context) => {
 	const params = {
 		TableName: process.env.tableName,
 		//'Key' Definies the partition key and sort key of the item to be retrieved
